bfa36eda907fdc7792358e3a95a03893021e0b3b
Seongbin Kim <seongbin9786@gmail.com>
2024-02-11 16:26:29 +0900

    chore(site.config.ts): 블로그 메타데이터 설정

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd9acbb64b0a24402a438165c07d5b4f2',
+  rootNotionPageId: 'f388d8be8b664889bf3ff0ac7b6c47cf',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
