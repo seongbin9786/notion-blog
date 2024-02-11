d8fec56040b0f93d1e163b77662a5745f9fd7101
Seongbin Kim <seongbin9786@gmail.com>
2024-02-11 17:44:19 +0900

    chore(site.config.ts): About, Contact 페이지 URL 설정

--- site.config.ts
+++ site.config.ts
@@ -11,10 +11,10 @@ export default siteConfig({
   // basic site info (required)
   name: 'JSQnA',
   domain: 'jsqna.vercel.app',
-  author: '구스(Seongbin Kim)',
+  author: 'Goose(김성빈)',
 
   // open graph metadata (optional)
-  description: 'Web Frontend Engineering',
+  description: '웹 FE 엔지니어 김성빈입니다!',
 
   // social usernames (optional)
   // twitter: '',
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default',
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'a1bef0b8e29d4315adccbee118559420'
+    },
+    {
+      title: 'Contact',
+      pageId: '23a10a7b946e4c589f9b74619828be51'
+    }
+  ]
 })
